475d0db5b71221fedca04165e73e27aa5eb5b19c
superkele <1622011654@qq.com>
2024-04-29 11:11:23 +0800

    refactor: 修改重构TranslatorFactory

---
+++ easy-translation-core/src/main/java/com/superkele/translation/core/translator/support/DefaultTranslatorFactory.java
@@ -0,0 +1,42 @@
+package com.superkele.translation.core.translator.support;
+
+import com.superkele.translation.core.translator.definition.ConfigurableTranslatorDefinitionFactory;
+import com.superkele.translation.core.translator.definition.TranslatorDefinition;
+import com.superkele.translation.core.translator.definition.TranslatorDefinitionRegistry;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class DefaultTranslatorFactory extends AbstractAutowireCapableTranslatorFactory
+        implements ConfigurableTranslatorDefinitionFactory, TranslatorDefinitionRegistry {
+
+    private final Map<String, TranslatorDefinition> translatorDefinitionMap = new ConcurrentHashMap<>();
+
+    @Override
+    public void register(String translatorName, TranslatorDefinition definition) {
+        translatorDefinitionMap.put(translatorName, definition);
+    }
+
+    @Override
+    public TranslatorDefinition getTranslatorDefinition(String translatorName) {
+        return Optional.ofNullable(translatorDefinitionMap.get(translatorName))
+                .orElseThrow(() -> new RuntimeException("No translator name '" + translatorName + "' is founded"));
+    }
+
+    @Override
+    public String[] getTranslatorNames() {
+        return translatorDefinitionMap.keySet().toArray(String[]::new);
+    }
+
+    @Override
+    public boolean containsTranslatorDefinition(String name) {
+        return translatorDefinitionMap.containsKey(name);
+    }
+
+
+    @Override
+    public TranslatorDefinition findTranslatorDefinition(String translatorName) {
+        return this.getTranslatorDefinition(translatorName);
+    }
+}
