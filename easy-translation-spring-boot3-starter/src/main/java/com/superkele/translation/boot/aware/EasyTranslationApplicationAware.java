87395246631ccc150b8d8ba46f75d2d776a69842
superkele <1622011654@qq.com>
2024-05-18 15:41:27 +0800

    refactor: 重构spring-boot3模块写法

--- easy-translation-spring-boot3-starter/src/main/java/com/superkele/translation/boot/aware/EasyTranslationApplicationAware.java
+++ easy-translation-spring-boot3-starter/src/main/java/com/superkele/translation/boot/aware/EasyTranslationApplicationAware.java
@@ -4,18 +4,18 @@ import com.superkele.translation.boot.global.TranslationGlobalInformation;
 import com.superkele.translation.core.config.Config;
 import com.superkele.translation.core.context.ConfigurableTransExecutorContext;
 import com.superkele.translation.core.context.support.DefaultTransExecutorContext;
+import com.superkele.translation.core.translator.support.DefaultTranslatorDefinitionReader;
 import com.superkele.translation.core.util.LogUtils;
 import org.springframework.beans.BeansException;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.Set;
 
 
 @Component("EasyTranslationApplicationAware")
-@Cacheable
 public class EasyTranslationApplicationAware implements ApplicationContextAware {
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
@@ -23,14 +23,17 @@ public class EasyTranslationApplicationAware implements ApplicationContextAware 
         if (executorContext instanceof DefaultTransExecutorContext) {
             DefaultTransExecutorContext defaultTransExecutorContext = (DefaultTransExecutorContext) executorContext;
             Config defaultTranslationConfig = applicationContext.getBean("defaultTranslationConfig", Config.class);
-            defaultTransExecutorContext.setConfig(defaultTranslationConfig);
             String[] packages = TranslationGlobalInformation.getPackages().stream().toArray(String[]::new);
-            defaultTransExecutorContext.setPackages(packages);
-            String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
-            Object[] invokeObjs = Arrays.stream(beanDefinitionNames)
+            DefaultTranslatorDefinitionReader reader = new DefaultTranslatorDefinitionReader(packages);
+            Set<Class<?>> translatorDeclaringClasses = reader.getTranslatorDeclaringClasses();
+            Object[] invokeObjs = translatorDeclaringClasses.stream()
+                    .map(clazz -> applicationContext.getBeanNamesForType(clazz))
+                    .flatMap(Arrays::stream)
                     .filter(beanName -> !"EasyTranslationApplicationAware".equals(beanName))
-                    .map(beanName -> applicationContext.getBean(beanName))
+                    .map(applicationContext::getBean)
                     .toArray(Object[]::new);
+            defaultTransExecutorContext.setDefinitionReader(reader);
+            defaultTransExecutorContext.setConfig(defaultTranslationConfig);
             defaultTransExecutorContext.setInvokeObjs(invokeObjs);
         }
         executorContext.refresh();
