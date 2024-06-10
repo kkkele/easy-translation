89c531786a3c0f707b043c87076cc79b02668ae8
superkele <1622011654@qq.com>
2024-06-10 14:57:41 +0800

    feat: 扩展@translator参数

--- easy-translation-spring-boot3-starter/src/main/java/com/superkele/translation/boot/aware/EasyTranslationApplicationAware.java
+++ easy-translation-spring-boot3-starter/src/main/java/com/superkele/translation/boot/aware/EasyTranslationApplicationAware.java
@@ -1,19 +1,16 @@
 package com.superkele.translation.boot.aware;
 
 import com.superkele.translation.boot.global.TranslationGlobalInformation;
+import com.superkele.translation.boot.invoker.SpringInvokeBeanFactory;
 import com.superkele.translation.core.config.Config;
 import com.superkele.translation.core.context.ConfigurableTranslatorContext;
 import com.superkele.translation.core.context.support.DefaultTranslatorContext;
-import com.superkele.translation.core.translator.support.DefaultTranslatorDefinitionReader;
 import com.superkele.translation.core.util.LogUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
-
-import java.util.Arrays;
-import java.util.Set;
 
 @Slf4j
 @Component("EasyTranslationApplicationAware")
@@ -24,20 +21,13 @@ public class EasyTranslationApplicationAware implements ApplicationContextAware 
         if (executorContext instanceof DefaultTranslatorContext) {
             DefaultTranslatorContext defaultTransExecutorContext = (DefaultTranslatorContext) executorContext;
             Config defaultTranslationConfig = applicationContext.getBean("defaultTranslationConfig", Config.class);
+            SpringInvokeBeanFactory invokeBeanFactory = applicationContext.getBean(SpringInvokeBeanFactory.class);
             String[] packages = TranslationGlobalInformation.getPackages().stream().toArray(String[]::new);
-            DefaultTranslatorDefinitionReader reader = new DefaultTranslatorDefinitionReader(packages);
-            Set<Class<?>> translatorDeclaringClasses = reader.getTranslatorDeclaringClasses();
-            Object[] invokeObjs = translatorDeclaringClasses.stream()
-                    .map(clazz -> applicationContext.getBeanNamesForType(clazz))
-                    .flatMap(Arrays::stream)
-                    .filter(beanName -> !"EasyTranslationApplicationAware".equals(beanName))
-                    .map(applicationContext::getBean)
-                    .toArray(Object[]::new);
-            defaultTransExecutorContext.setDefinitionReader(reader);
+            defaultTransExecutorContext.setBasePackages(packages);
             defaultTransExecutorContext.setConfig(defaultTranslationConfig);
-            defaultTransExecutorContext.setInvokeObjs(invokeObjs);
+            defaultTransExecutorContext.setInvokeBeanFactory(invokeBeanFactory);
         }
         executorContext.refresh();
-        LogUtils.debug(log::debug,"executorContext refresh success");
+        LogUtils.debug(log::debug, "executorContext refresh success");
     }
 }
--- easy-translation-spring-boot3-starter/src/main/java/com/superkele/translation/boot/selector/EnableTranslationImportSelector.java
+++ easy-translation-spring-boot3-starter/src/main/java/com/superkele/translation/boot/selector/EnableTranslationImportSelector.java
@@ -4,6 +4,7 @@ package com.superkele.translation.boot.selector;
 import com.superkele.translation.boot.aware.EasyTranslationApplicationAware;
 import com.superkele.translation.boot.config.EasyTranslationBaseConfig;
 import com.superkele.translation.boot.config.EasyTranslationInterceptorConfig;
+import com.superkele.translation.boot.invoker.SpringInvokeBeanFactory;
 import org.springframework.context.annotation.ImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
@@ -18,7 +19,9 @@ public class EnableTranslationImportSelector implements ImportSelector {
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
         return new String[]{EasyTranslationBaseConfig.class.getName(),
                 EasyTranslationInterceptorConfig.class.getName(),
-                EasyTranslationApplicationAware.class.getName()};
+                EasyTranslationApplicationAware.class.getName(),
+                SpringInvokeBeanFactory.class.getName()
+        };
     }
 
 
