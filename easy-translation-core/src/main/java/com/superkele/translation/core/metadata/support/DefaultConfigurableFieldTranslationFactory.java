4958120d6b16555418df8c333307c325a0be2753
superkele <1622011654@qq.com>
2024-06-27 14:16:41 +0800

    pref: 优化代码写法

--- easy-translation-core/src/main/java/com/superkele/translation/core/metadata/support/DefaultConfigurableFieldTranslationFactory.java
+++ easy-translation-core/src/main/java/com/superkele/translation/core/metadata/support/DefaultConfigurableFieldTranslationFactory.java
@@ -1,6 +1,5 @@
 package com.superkele.translation.core.metadata.support;
 
-import com.superkele.translation.core.context.ConfigurableTranslatorContext;
 import com.superkele.translation.core.context.support.AbstractTranslatorContext;
 import com.superkele.translation.core.mapping.ParamHandlerResolver;
 import com.superkele.translation.core.mapping.ResultHandlerResolver;
@@ -11,9 +10,6 @@ import com.superkele.translation.core.metadata.FieldTranslationRegistry;
 
 public class DefaultConfigurableFieldTranslationFactory implements ConfigurableFieldTranslationFactory, FieldTranslationRegistry {
 
-    private final AbstractTranslatorContext translatorContext;
-    private final ParamHandlerResolver paramHandlerResolver;
-    private final ResultHandlerResolver resultHandlerResolver;
     private final String[] basePath;
     protected DefaultFieldTranslationFactory fieldTranslationFactory;
     private FieldTranslationBuilder builder;
@@ -21,9 +17,6 @@ public class DefaultConfigurableFieldTranslationFactory implements ConfigurableF
     public DefaultConfigurableFieldTranslationFactory(AbstractTranslatorContext translatorContext,
                                                       ParamHandlerResolver paramHandlerResolver,
                                                       ResultHandlerResolver resultHandlerResolver, String... basePath) {
-        this.translatorContext = translatorContext;
-        this.paramHandlerResolver = paramHandlerResolver;
-        this.resultHandlerResolver = resultHandlerResolver;
         this.basePath = basePath;
         translatorContext.register(context -> {
             builder = new DefaultMappingFiledTranslationBuilder(translatorContext.getTranslatorFactory(), paramHandlerResolver, resultHandlerResolver);
@@ -34,7 +27,7 @@ public class DefaultConfigurableFieldTranslationFactory implements ConfigurableF
     @Override
     public void refresh() {
         fieldTranslationFactory = new DefaultFieldTranslationFactory(builder);
-        DefaultFiledTranslationReader defaultFiledTranslationReader = new DefaultFiledTranslationReader(fieldTranslationFactory, builder);
+        DefaultFiledTranslationReader defaultFiledTranslationReader = new DefaultFiledTranslationReader(this, builder);
         defaultFiledTranslationReader.load(basePath);
     }
 
